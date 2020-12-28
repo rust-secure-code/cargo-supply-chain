82665d7a4780964b04e85e9be58c75261bbb2128
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2020-12-28 16:50:00 +0100

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -65,8 +65,15 @@ fn parse_max_age(text: &str) -> Result<Duration, humantime::DurationError> {
 /// Separates arguments intended for us and for cargo-metadata
 fn get_grouped_args() -> (Vec<OsString>, Vec<String>) {
     // Everything before "--" should be parsed, and everything after it should be passed to cargo-metadata
-    let mut supply_args: Vec<OsString> = std::env::args_os().skip(1).take_while(|x| x != "--").collect();
-    let metadata_args = std::env::args().skip(1).skip_while(|x| x != "--").skip(1).collect();
+    let mut supply_args: Vec<OsString> = std::env::args_os()
+        .skip(1)
+        .take_while(|x| x != "--")
+        .collect();
+    let metadata_args = std::env::args()
+        .skip(1)
+        .skip_while(|x| x != "--")
+        .skip(1)
+        .collect();
     // When invoked via `cargo supply-chain update`, Cargo passes the arguments it receives verbatim.
     // So instead of "update" our binary receives "supply-chain update".
     // We ignore the "supply-chain" in the beginning if it's present.
