a1bf7a383f3a8e70c0e19232372e48172becb958
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2020-12-29 21:40:07 +0100

    cargo fmt

--- src/subcommands/update.rs
+++ src/subcommands/update.rs
@@ -6,17 +6,19 @@ pub fn update(max_age: std::time::Duration) {
     let mut client = RateLimitedClient::new();
 
     match cache.download(&mut client, max_age) {
-        Ok(state) => {
-            match state{
-                DownloadState::Fresh => println!("No updates found"),
-                DownloadState::Expired => println!("Successfully updated to the newest daily data dump."),
-                DownloadState::Stale => {
-                    println!("Downloaded latest daily data dump.");
-                    println!("  Warning: it matches the previous version that was considered outdated.");
-                    std::process::exit(1);
-                }
+        Ok(state) => match state {
+            DownloadState::Fresh => println!("No updates found"),
+            DownloadState::Expired => {
+                println!("Successfully updated to the newest daily data dump.")
             }
-        }
+            DownloadState::Stale => {
+                println!("Downloaded latest daily data dump.");
+                println!(
+                    "  Warning: it matches the previous version that was considered outdated."
+                );
+                std::process::exit(1);
+            }
+        },
         Err(error) => {
             println!("Could not update to the latest daily data dump!\n{}", error);
         }
