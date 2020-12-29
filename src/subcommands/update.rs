0aa94623f37e4b292efc47f42d39ba072c889d4c
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2020-12-29 21:39:31 +0100

    Present db download error to the user instead of panicking

--- src/subcommands/update.rs
+++ src/subcommands/update.rs
@@ -5,13 +5,20 @@ pub fn update(max_age: std::time::Duration) {
     let mut cache = CratesCache::new();
     let mut client = RateLimitedClient::new();
 
-    match cache.download(&mut client, max_age).unwrap() {
-        DownloadState::Fresh => println!("No updates found"),
-        DownloadState::Expired => println!("Successfully updated to the newest daily data dump."),
-        DownloadState::Stale => {
-            println!("Downloaded latest daily data dump.");
-            println!("  Warning: it matches the previous version that was considered outdated.");
-            std::process::exit(1);
+    match cache.download(&mut client, max_age) {
+        Ok(state) => {
+            match state{
+                DownloadState::Fresh => println!("No updates found"),
+                DownloadState::Expired => println!("Successfully updated to the newest daily data dump."),
+                DownloadState::Stale => {
+                    println!("Downloaded latest daily data dump.");
+                    println!("  Warning: it matches the previous version that was considered outdated.");
+                    std::process::exit(1);
+                }
+            }
+        }
+        Err(error) => {
+            println!("Could not update to the latest daily data dump!\n{}", error);
         }
     }
 }
