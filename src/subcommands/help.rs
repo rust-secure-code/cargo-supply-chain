efe284d1394d4475506bb29759a831b60cd0c4f9
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2021-02-10 18:07:28 +0100

    Prepare help subcommand handler for multiple commands

--- src/subcommands/help.rs
+++ src/subcommands/help.rs
@@ -4,7 +4,10 @@ use crate::CLI_HELP;
 use std::process;
 
 /// Provides help infomation which proceeds to exit
-pub fn help() {
-    println!("{}", CLI_HELP);
+pub fn help(command: Option<String>) {
+    match command {
+        None => println!("{}", CLI_HELP),
+        Some(Command) => todo!(),
+    }
     process::exit(0)
 }
