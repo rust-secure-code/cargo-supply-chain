a4679e3850bac516e5091a41470a0e9d0e453f88
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2021-02-10 18:41:39 +0100

    Match on the subcommand given

--- src/subcommands/help.rs
+++ src/subcommands/help.rs
@@ -6,8 +6,11 @@ use std::process;
 /// Provides help infomation which proceeds to exit
 pub fn help(command: Option<&str>) {
     match command {
+        Some("crates") => todo!(),
+        Some("publishers") => todo!(),
+        Some("update") => todo!(),
+        Some(_) => println!("{}", CLI_HELP),
         None => println!("{}", CLI_HELP),
-        Some(Command) => todo!(),
     }
     process::exit(0)
 }
