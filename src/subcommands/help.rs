0b844533f093d9902202fc47cf42b389a5fb6ed5
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2021-02-10 18:51:04 +0100

    Complain about unknown subcommand in help

--- src/subcommands/help.rs
+++ src/subcommands/help.rs
@@ -6,11 +6,15 @@ use std::process;
 /// Provides help infomation which proceeds to exit
 pub fn help(command: Option<&str>) {
     match command {
+        None => println!("{}", CLI_HELP),
         Some("crates") => todo!(),
         Some("publishers") => todo!(),
         Some("update") => todo!(),
-        Some(_) => println!("{}", CLI_HELP),
-        None => println!("{}", CLI_HELP),
+        Some(command) => {
+            println!("Unknown subcommand: {}\n", command);
+            println!("{}", CLI_HELP);
+            process::exit(1)
+        }
     }
     process::exit(0)
 }
