b5d6ad20c078a89ce37f07df93e7256b2d5ca0e1
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2021-05-01 12:38:28 +0200

    Add initial help text for the JSON subcommand

--- src/subcommands/help.rs
+++ src/subcommands/help.rs
@@ -7,8 +7,9 @@ use std::process;
 pub fn help(command: Option<&str>) {
     match command {
         None => println!("{}", CLI_HELP),
-        Some("crates") => println!("{}", CRATES_HELP),
         Some("publishers") => println!("{}", PUBLISHERS_HELP),
+        Some("crates") => println!("{}", CRATES_HELP),
+        Some("json") => println!("{}", JSON_HELP),
         Some("update") => println!("{}", UPDATE_HELP),
         Some(command) => {
             println!("Unknown subcommand: {}\n", command);
@@ -61,6 +62,35 @@ OPTIONS:
 Any arguments after the `--` will be passed to `cargo metadata`, for example:
   cargo supply-chain crates -- --filter-platform=x86_64-unknown-linux-gnu
 See `cargo metadata --help` for a list of flags it supports.";
+
+const JSON_HELP: &str = "Detailed info on publishers of all crates in the dependency graph, in JSON
+
+The JSON schema is provided below, but the output is designed to be self-explanatory.
+
+If a local cache created by 'update' subcommand is present and up to date,
+it will be used. Otherwise live data will be fetched from the crates.io API.
+
+It's not guaranteed that the local cache will be used if '--cache-max-age' is
+set to less than 48 hours, even if you've run 'update' subcommand just now.
+That's because crates.io database dumps may not be updated every single day.
+
+Note that detailed information on the origin of crates outside of crates.io is not
+provided. You can obtain this info from 'cargo metadata' that ships with Cargo,
+or use 'cargo deny' to define a custom policy regarding crate sources.
+
+USAGE:
+  cargo supply-chain json [OPTIONS...] [-- CARGO_METADATA_OPTIONS...]
+
+OPTIONS:
+  --cache-max-age  The cache will be considered valid while younger than specified.
+                   The format is a human readable duration such as `1w` or `1d 6h`.
+                   If not specified, the cache is considered valid for 48 hours.
+
+Any arguments after the `--` will be passed to `cargo metadata`, for example:
+  cargo supply-chain crates -- --filter-platform=x86_64-unknown-linux-gnu
+See `cargo metadata --help` for a list of flags it supports.
+
+TODO: schema";
 
 const UPDATE_HELP: &str = "Download the latest daily dump from crates.io to speed up other commands
 
