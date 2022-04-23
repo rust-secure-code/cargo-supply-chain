56d3147d97eeb549e3cfbb0c44c860f0ef233056
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2022-04-23 13:09:37 +0200

    Use anyhow::Context to see what it looks like

--- src/subcommands/update.rs
+++ src/subcommands/update.rs
@@ -1,12 +1,12 @@
 use crate::api_client::RateLimitedClient;
 use crate::crates_cache::{CratesCache, DownloadState};
-use anyhow::bail;
+use anyhow::{bail, Context};
 
 pub fn update(max_age: std::time::Duration) -> Result<(), anyhow::Error> {
     let mut cache = CratesCache::new();
     let mut client = RateLimitedClient::new();
 
-    match cache.download(&mut client, max_age) {
+    match cache.download(&mut client, max_age).context("Could not update to the latest daily data dump") {
         Ok(state) => match state {
             DownloadState::Fresh => eprintln!("No updates found"),
             DownloadState::Expired => {
@@ -14,7 +14,7 @@ pub fn update(max_age: std::time::Duration) -> Result<(), anyhow::Error> {
             }
             DownloadState::Stale => bail!("Downloaded latest daily data dump.\n  Warning: it matches the previous version that was considered outdated.")
         },
-        Err(error) => bail!("Could not update to the latest daily data dump!\n{}", error)
+        Err(error) => bail!(error)
     }
     Ok(())
 }
